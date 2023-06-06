705c5b1e5551cd0a6b57be15b13fbd4eafaf7a7b
MANN MISTRY <mn732058@dal.ca>
2023-06-06 18:28:08 -0300

    moved backend code from git-colab-backend to gc-server

---
+++ gc-server/src/main/java/com/example/gitcolabbackend/GitColabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitcolabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitColabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitColabBackendApplication.class, args);
+    }
+
+}
