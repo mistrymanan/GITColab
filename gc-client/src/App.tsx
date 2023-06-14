af852ec542c22d7ab88be1878a691eaf30a159d9
Uchenna Chima <uc388002@dal.ca>
2023-06-14 11:34:28 -0300

    added routing

--- gc-client/src/App.tsx
+++ gc-client/src/App.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import {Container} from 'react-bootstrap';
 import Profile from './view/Profile/Profile';
+import Dashboard from './view/Dashboard/Dashboard';
+import Explore from './view/Explore/Explore';
+import Integration from './view/Integration/Integration';
+import Landing from './view/Landing/Landing';
+import HomeScreen from './view/HomeScreen/HomeScreen';
 
 function App() {
   return (
-    <>
-    <Header/>
-      <Container>
-        <Profile/>
-      </Container>
-    <Footer/>
-    </>
+    <Router>
+      <Header/>
+
+        <Container>
+          <Routes>
+            <Route path='/' Component={HomeScreen}/>
+            <Route path='/profile' Component={Profile}/>
+            <Route path='/dashboard' Component={Dashboard}/>
+            <Route path='/explore' Component={Explore}/>
+            <Route path='/integration' Component={Integration}/>
+            <Route path='/landing' Component={Landing}/>
+          </Routes>
+        </Container>
+
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ gc-client/src/view/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,8 @@
+const HomeScreen = () => {
+
+    return (
+        <h1>HomeScreen View</h1>
+    )
+}
+
+export default HomeScreen;
