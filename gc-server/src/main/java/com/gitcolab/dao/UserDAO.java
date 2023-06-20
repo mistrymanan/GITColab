3e3a3620790b440674504d1e958afd6361327468
MANN MISTRY <mn732058@dal.ca>
2023-06-20 10:53:50 -0300

    added User DAO and It's Test

--- gc-server/src/main/java/com/gitcolab/dao/UserDAO.java
+++ gc-server/src/main/java/com/gitcolab/dao/UserDAO.java
@@ -1,4 +1,15 @@
 package com.gitcolab.dao;
 
-public class UserDAO {
+import com.gitcolab.entity.User;
+
+import java.util.Optional;
+
+public interface UserDAO extends DAO  {
+    Optional<User> getUserByUserName(String username);
+    Optional<User> getUserByEmail(String email);
+
+    public boolean existsByUsername(String username);
+
+    public boolean existsByEmail(String email);
+
 }
