6cb6d8df182942515898561256d947851de9da98
MANN MISTRY <mn732058@dal.ca>
2023-06-20 11:18:18 -0300

    Added JDBC implementation of UserDAO and also added Utility UserRowMapper

--- gc-server/src/main/java/com/gitcolab/repositories/UserRepository.java
+++ gc-server/src/main/java/com/gitcolab/repositories/UserRepository.java
@@ -1,4 +1,18 @@
 package com.gitcolab.repositories;
 
-public interface UserRepository {
+import com.gitcolab.entity.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository{
+    int save(User user);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findById(long id);
 }
