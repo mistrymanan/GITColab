018414765019e55fb8c1b4ecc74d8e5047728025
Keyur Khant <keyur.khant@edba.io>
2023-07-03 00:15:29 -0300

    fix: footer fixes

--- gc-client/src/App.tsx
+++ gc-client/src/App.tsx
@@ -3,7 +3,6 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 import PageRoutes from './routes/routes';
 import {Container} from 'react-bootstrap';
-import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 const App = () => {
@@ -12,7 +11,6 @@ const App = () => {
         <Container style={{maxWidth:'100vw', padding:0}}>
           <Header />
           <PageRoutes />
-          {/* <Footer /> */}
         </Container>
     </Router>
   );
