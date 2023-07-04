e9d3e5c1c75fe9c068d245db3456963ce7467c9a
Keyur Pradipbhai Khant <ky468409@dal.ca>
2023-07-04 13:20:25 -0300

    fix: header fixes

--- gc-client/src/components/Header/Header.tsx
+++ gc-client/src/components/Header/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
                 <Container className=''>
 
                     <LinkContainer to='/'>
-                        <Navbar.Brand >Github Collab</Navbar.Brand>
+                        <Navbar.Brand >Github Colab</Navbar.Brand>
                     </LinkContainer>
 
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
