23054daa21d6a5df7c2ded729d915ec0392fcd3a
Parth Mehta <parthmehtaprog@gmail.com>
2023-07-21 10:57:26 -0700

    Added Card in Explore API.

--- gc-client/src/view/Explore/Explore.tsx
+++ gc-client/src/view/Explore/Explore.tsx
@@ -1,8 +1,37 @@
+import { Container } from "react-bootstrap";
+import {
+    MDBBtn,
+    MDBCard,
+    MDBCardBody,
+    MDBCardImage,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCol
+  } from 'mdbreact';
+
 const Explore = () => {
+    // Use Card
     return (
-        <>
-            <h1>Explore View</h1>
-        </>
+        <div>
+            <Container>
+                <MDBCol>
+                    <MDBCard style={{ width: "22rem" }}>
+                        <MDBCardBody>
+                            <MDBCardTitle> 
+                                Card Title
+                            </MDBCardTitle>
+                            <MDBCardText>
+                                Card Textssssssssssss    
+                            </MDBCardText>
+                            <MDBBtn>
+                                Card Button    
+                            </MDBBtn>
+                            <MDBCardImage src="src\images\image-1.jpg" />
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+            </Container>
+        </div>
     )
 }
 
