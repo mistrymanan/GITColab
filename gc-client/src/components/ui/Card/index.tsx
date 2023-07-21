fd258ff6c2e482459c8ff7cab5e7a1e9ace459ba
Parth Mehta <parthmehtaprog@gmail.com>
2023-07-21 11:57:30 -0700

    Feat: Added DropDown Menu and Button in Ui & Cards.

--- gc-client/src/components/ui/Card/index.tsx
+++ gc-client/src/components/ui/Card/index.tsx
@@ -1,19 +1,22 @@
-import { Card } from "react-bootstrap"
+import { Card, Button } from "react-bootstrap"
 
 const CardComponent = (props: any) => {
   return (
     <Card className="my-3 p-3 rounded" border="secondary" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png" alt="Repo Image" />
-
+      <a href="#" target="_blank" rel="noopener noreferrer">
+          <Card.Img variant="top" src="https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png" alt="Repo Image" />
+      </a>
       <Card.Body>
-        <Card.Title><h4>Repo Title</h4></Card.Title>
-        <Card.Text className="my-1"><h5>Repo Name - Repo Owner</h5></Card.Text>
-        <Card.Text as='p'>Repo Description</Card.Text>
-        <Card.Text as='p'>
-          5 <i className="fas fa-solid fa-star" />'s
-          5 <i className="fas fa-solid fa-code-branch" />'s
-          5 <i className="fas fa-solid fa-exclamation-circle" />'s
-        </Card.Text>
+          <Card.Title><h4>GitColab</h4></Card.Title>
+          <Card.Text className="my-1">keyurkhant</Card.Text>
+          <Card.Text as='p'>Test our Cards</Card.Text>
+          <Card.Text as='p'>
+            5 <i className="fas fa-solid fa-star" />'s
+            5 <i className="fas fa-solid fa-code-branch" />'s
+            5 <i className="fas fa-solid fa-exclamation-circle" />'s
+          </Card.Text>
+          
+          <Button variant="primary" href="#" target="_blank">Send Request</Button>
       </Card.Body>
     </Card>
   )
--- gc-client/src/view/Explore/Explore.tsx
+++ gc-client/src/view/Explore/Explore.tsx
@@ -1,27 +1,49 @@
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Dropdown } from "react-bootstrap";
 import CardComponent from "../../components/ui/Card";
 
 const Explore = () => {
+    const [showAddProjectFlow, setShowSearchProjectFlow] = useState(false);
+
+    const searchProjectFlowHandler = () => {
+        setShowSearchProjectFlow(true);
+    };
+
     return (
         <>
             <Container className="mt-3">
+                <div className="d-flex flex-row my-2 justify-content-between">
+                    <h1 className='py-3'> Explore New Projects </h1>
+
+                    <Dropdown onClick={searchProjectFlowHandler}>
+                        <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                            Search Project
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">1st Connection</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">2nd Connection</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">3rd Connection</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
                 <div className="d-flex flex-wrap justify-content-between my-2">
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                <CardComponent/>
-                </div> 
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                    <CardComponent />
+                </div>
             </Container>
         </>
     )
