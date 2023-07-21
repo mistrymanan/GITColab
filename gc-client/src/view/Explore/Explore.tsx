767ef2fcd7dfb78c410078215f72a4bf92271392
Parth Mehta <parthmehtaprog@gmail.com>
2023-07-21 10:58:20 -0700

    Fix: Card in Explore API; Using react-bootstrap.

--- gc-client/src/view/Explore/Explore.tsx
+++ gc-client/src/view/Explore/Explore.tsx
@@ -1,37 +1,26 @@
 import { Container } from "react-bootstrap";
-import {
-    MDBBtn,
-    MDBCard,
-    MDBCardBody,
-    MDBCardImage,
-    MDBCardTitle,
-    MDBCardText,
-    MDBCol
-  } from 'mdbreact';
+import { Card } from "react-bootstrap"
 
 const Explore = () => {
-    // Use Card
     return (
-        <div>
+        <>
             <Container>
-                <MDBCol>
-                    <MDBCard style={{ width: "22rem" }}>
-                        <MDBCardBody>
-                            <MDBCardTitle> 
-                                Card Title
-                            </MDBCardTitle>
-                            <MDBCardText>
-                                Card Textssssssssssss    
-                            </MDBCardText>
-                            <MDBBtn>
-                                Card Button    
-                            </MDBBtn>
-                            <MDBCardImage src="src\images\image-1.jpg" />
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
+                <Card className="my-3 p-3 rounded " border="primary" style={{ width: '18rem' }}>
+                    <Card.Img src="/src/images/image-1.jpg" variant="top" />
+
+                    <Card.Body>
+                        <Card.Title><h4>Repo Title</h4></Card.Title>
+                        <Card.Text className="my-1"><h5>Repo Name - Repo Owner</h5></Card.Text>
+                        <Card.Text as='p'>Repo Description</Card.Text>
+                        <Card.Text as='p'>
+                            5 <i className="fas fa-solid fa-star" />'s
+                            5 <i className="fas fa-solid fa-code-branch" />'s
+                            5 <i className="fas fa-solid fa-exclamation-circle" />'s
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
             </Container>
-        </div>
+        </>
     )
 }
 
